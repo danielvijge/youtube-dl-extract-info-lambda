f8c2559b93d00f567fbaa2bc395b31bf3c66ecc6
Daniel Vijge <danielvijge@users.noreply.github.com>
2019-05-19 14:31:45 +0200

    Change to Python3

--- extract_info.py
+++ extract_info.py
@@ -1,7 +1,7 @@
 from __future__ import unicode_literals
 from youtube_dl import YoutubeDL
 import json
-import urllib2
+import urllib.parse
 
 from youtube_dl.utils import (
     DownloadError
@@ -11,7 +11,7 @@ def lambda_handler(event, context):
     lambda_response =  {
         "isBase64Encoded": False,
         "statusCode": 200,
-        "headers": {},
+        "headers": {"Content-Type": "application/json"},
         "body": "{}"
     }
 
@@ -19,7 +19,7 @@ def lambda_handler(event, context):
        lambda_response['statusCode'] = 400
        return lambda_response
     
-    url = urllib2.unquote(event['queryStringParameters']['url']).decode('utf8')
+    url = urllib.parse.unquote(event['queryStringParameters']['url'])
     
     ydl_opts = {
          'skip_download': True,
@@ -39,10 +39,11 @@ def lambda_handler(event, context):
 def test(name, url, statuscode):
     test_case = lambda_handler({'queryStringParameters': {'url': url}}, None)
     if test_case['statusCode'] == statuscode:
-        print "[OK] '%s' passed" % name
+        print("[OK] '%s' passed" % name)
     else:
-        print "[FAIL] '%s' failed, retured %d, %d expected" % (name, test_case['statusCode'], statuscode)
+        print("[FAIL] '%s' failed, retured %d, %d expected" % (name, test_case['statusCode'], statuscode))
 
+print('Running unit tests...')
 test('extract info from Youtube', 'https://www.youtube.com/watch?v=dQw4w9WgXcQ', 200)
 test('invalid url', 'https://www.youtube.com/watch?v=00000000000', 404)
 test('invalid extractor', 'https://www.example.com/', 404)
